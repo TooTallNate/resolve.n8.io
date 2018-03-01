376cba7fffe4934836bd3edde5465f39978e3078
Nathan Rajlich <nathan@tootallnate.net>
2018-02-28 16:02:18 -0800

    Refactor to `resolve.now.sh`

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
-const { resolve4 } = require('mz/dns');
+const { parse } = require('url');
+const resolve = require('@zeit/dns-cached-resolve');
 
 module.exports = async (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
-  const ips = await resolve4('alias.zeit.co');
-  return ips[0];
+  const parsed = parse(req.url);
+  const host = parsed.pathname.substring(1).trim();
+  if (!host) {
+    res.statusCode = 404;
+    res.end();
+  }
+  console.log('Resolving %j', host);
+  try {
+    const ip = await resolve(host);
+    return ip;
+  } catch (err) {
+    if (err.code === 'ENOTFOUND') {
+      res.statusCode = 404;
+      res.end();
+    } else {
+      throw err;
+    }
+  }
 };
